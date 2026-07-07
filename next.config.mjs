6891a0dd820d332ef4494db30ad39989825b3d61
First China Trip Kit <china.travel.kit@gmail.com>
2026-07-07 22:12:46 +0800

    Redirect apex domain to www

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "firstchinatripkit.com",
+          },
+        ],
+        destination: "https://www.firstchinatripkit.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
